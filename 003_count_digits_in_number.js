73cdd9ef34bedd7116df598c44d533ca429b24d4
DESKTOP-BOJDC0N\pc <manishkumar12340@gmail.com>
2026-03-29 23:10:12 +0530

    count digits in a number

---
+++ 003_count_digits_in_number.js
@@ -0,0 +1,25 @@
+/*
+Question 3: Count number of digits in a numiber
+*/
+
+/** 
+ * @param {number}
+ * @return {number}
+ **/
+
+function countDigits(num){
+    count=0
+
+    do{
+        count++
+        num=Math.floor(num/10)
+    }while(num>0)
+
+    return count
+}
+
+console.log(countDigits(34627))
+console.log(countDigits(0))
+console.log(countDigits(12))
+console.log(countDigits(1903))
+
